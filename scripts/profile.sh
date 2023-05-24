ae0ef95d1ca4f95cdf9974ec99b9f630f9db3155
SpringWave1 <534239589@qq.com>
2023-05-24 21:39:18 +0800

    latest profiler

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -13,7 +13,7 @@ for batch_size in 1 2 4 8 16 32
 do
     for prompt_length in 512 128 
     do
-        for model_size in 30b
+        for model_size in 13b 30b 66b
         do
             python3 profile_lat.py --batch-size $batch_size --input-seq-length $prompt_length --past-seq-length 0 \
              --generated-seq-length 1 --step 20 --warmup 2 --repeat 10 --model-size $model_size
@@ -37,7 +37,7 @@ done
 
 echo "run 176b"
 # for model 176b
-for batch_size in 16 8 4 2 1
+for batch_size in 32 16 8 4 2 1
 do
     for past_seq_length in 128 512
     do
