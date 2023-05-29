ba78afb1c91f43f843299ff077430f5d5124c181
SpringWave1 <534239589@qq.com>
2023-05-29 18:04:16 +0800

    add some records

---
+++ result/30b_result/sols_opt_30b_Tesla_T4_4/script.sh
@@ -0,0 +1,12 @@
+# export CUDA_VISIBLE_DEVICES=3
+export TOKENIZERS_PARALLELISM=0
+export OMP_NUM_THREADS=32
+available_methods=('uniform')
+for i in "${!available_methods[@]}"
+do  
+    torchrun --nnodes=1 --nproc_per_node=4 --master_port 6666 main_p2p.py --model_name opt --model_size 30b --method ${available_methods[i]} \
+        2>&1 | tee "${available_methods[i]}_30b_homo"
+    pkill torchrun
+    pkill python3
+done
+
---
+++ result/66b_result/sols_opt_66b_Tesla_V100-SXM2-32GB_2_NVIDIA_A100-SXM4-40GB_2/script.sh
@@ -0,0 +1,30 @@
+export CUDA_VISIBLE_DEVICES=2,3
+export TOKENIZERS_PARALLELISM=0
+export OMP_NUM_THREADS=32
+available_methods=('uniform')
+STRAT_FILE_NAME="sols_opt_66b_Tesla_V100-SXM2-32GB_2_NVIDIA_A100-SXM4-40GB_2"
+# exp1
+rank=0
+MASTER_ADDR=***REMOVED***
+model_size="66b"
+for i in "${!available_methods[@]}"
+do  
+    torchrun --nnodes=2 --nproc_per_node=2 --master_addr $MASTER_ADDR --master_port 6666 --node_rank=${rank} main_p2p.py \
+     --model_name opt --model_size ${model_size} --method ${available_methods[i]} --strat_file_name $STRAT_FILE_NAME \
+        2>&1 | tee "${available_methods[i]}_${model_size}"
+    pkill torchrun
+    pkill python3
+done
+
+# rank=1
+# MASTER_ADDR=***REMOVED***
+# available_methods=('adaqpipe')
+# for i in "${!available_methods[@]}"
+# do  
+#     torchrun --nnodes=2 --nproc_per_node=2 --master_addr net-g14 --master_port 6666 --node_rank=${rank} main_p2p.py \
+#      --model_name opt --model_size ${model_size} --method ${available_methods[i]} --strat_file_name $STRAT_FILE_NAME \
+#         2>&1 | tee "${available_methods[i]}_${model_size}"
+#     pkill torchrun
+#     pkill python3
+# done
+
