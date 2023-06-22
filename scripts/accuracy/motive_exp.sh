f611d20fa1c0a856845bf998f8947eb5e53f3cb0
SpringWave1 <534239589@qq.com>
2023-06-22 14:39:18 +0800

    before table1 correct

--- scripts/accuracy/motive_exp.sh
+++ scripts/accuracy/motive_exp.sh
@@ -1,22 +1,26 @@
 storage_path="${PWD}/motive_mix"
 # test
 model_name=bloom
-model_size=560m
+model_size="560m"
 # bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --storage_path $storage_path
 # bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --mixed --storage_path $storage_path
 # run multiple mixed-precision result
 model_name=bloom
-model_size=3b
+model_size="3b"
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --mixed --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 4 --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 4 --mixed --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 8 --storage_path $storage_path
 
+bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 16 --storage_path $storage_path
+
 model_name=opt
-model_size=1.3b
+model_size="1.3b"
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 3 --mixed --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 4 --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 4 --mixed --storage_path $storage_path
 bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 8 --storage_path $storage_path
+
+bash test_plan_acc_cmd.sh --model_name $model_name --model_size $model_size --wbit 16 --storage_path $storage_path
