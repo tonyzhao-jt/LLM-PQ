6423f5d2b19e0ed753e0a86107b5e238383e5fb4
SpringWave1 <534239589@qq.com>
2023-07-21 11:28:50 +0800

    change some path directive

--- run_docker.sh
+++ run_docker.sh
@@ -5,12 +5,12 @@ DATA_PATH='/data'
 ROOT_PATH=$(realpath $(dirname $0))
 echo "Test project path: $ROOT_PATH"
 # deal with the permision problem
-docker build -t juntao/qpipe:0.1 .
-CONT_NAME="qpipe" 
+CONT_NAME="shaq" 
+docker build -t juntao/${CONT_NAME}:0.1 .
 # run if the docker container is not running but has been created
 if [ "$(docker ps -aq -f name=$CONT_NAME)" ]; then
     echo "Container $CONT_NAME is running"
     docker start $CONT_NAME
     exit 0
 fi
-docker run --net=host --gpus all --name $CONT_NAME -v $DATA_PATH:/data -v $ROOT_PATH:/workspace/qpipe -it juntao/qpipe:0.1 
+docker run --net=host --gpus all --name $CONT_NAME -v $DATA_PATH:/data -v $ROOT_PATH:/workspace/${CONT_NAME} -it juntao/${CONT_NAME}:0.1 
