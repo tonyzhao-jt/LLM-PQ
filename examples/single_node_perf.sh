eba0cee7c184d9c978d0fea75d72dd916118a2db
SpringWave1 <534239589@qq.com>
2023-07-21 15:03:38 +0800

    fixed some bugs for entrypoints

--- examples/single_node_perf.sh
+++ examples/single_node_perf.sh
@@ -1,15 +1,22 @@
-export CUDA_VISIBLE_DEVICES=3
-available_methods=('adabits') # from adabits, adaqpipe, uniform, pipeedge
-# A100:
-# STRAT_FILE_NAME="sols_opt_13b_NVIDIA_A100-SXM4-40GB_1"
-
-# V100: 
-# be careful when using V100, you need to recompile the bitsandbytes
-# refer LPTorch
+method='adabits' # from adabits, shaq, uniform, pipeedge
 STRAT_FILE_NAME="sols_opt_13b_Tesla_V100-SXM2-32GB_1"
-export TRANSFORMERS_CACHE='/data/llms/'
+export TOKENIZERS_PARALLELISM=false
+export TRANSFORMERS_CACHE='***REMOVED***llms/'
+export CALIB_ROOT_FOLDER="${ROOT_DIR}/examples/"
+export NP_WEIGHT_FOLDER="${TRANSFORMERS_CACHE}/converted_weights_np/"
+# CPU Not Enough
+export LOAD_IN_NP="1"
+MODEL_NAME='opt'
+MODEL_SIZE="13b"
 mkdir SINGLE_NODE_PERF_RESULT
 
+shaq-dist --master_port 6666 \
+    --model_name $MODEL_NAME --model_size $MODEL_SIZE\
+    --method $method \
+    --strat_file_name $STRAT_FILE_NAME \
+    2>&1 | tee "./SINGLE_NODE_PERF_RESULT/${available_methods[i]}_13b.txt"
+pkill torchrun
+pkill python3
 
 # Other Notice: 
     # Please run weight_convert.py first
@@ -19,16 +26,6 @@ mkdir SINGLE_NODE_PERF_RESULT
 # python3 check_strat.py --model_name opt --model_size 13b --method ${available_methods[i]} \
 #     --strat_file_name $STRAT_FILE_NAME 
 
-# A100-Single node
-for i in "${!available_methods[@]}"
-do  
-    shaq-dist --master_port 6666 main.py \
-    --model_name opt --model_size 13b --method ${available_methods[i]} \
-    --strat_file_name $STRAT_FILE_NAME \
-        2>&1 | tee "./SINGLE_NODE_PERF_RESULT/${available_methods[i]}_13b.txt"
-    pkill torchrun
-    pkill python3
-done
 
 # V100-Single node
 # available_methods=('adaqpipe')
